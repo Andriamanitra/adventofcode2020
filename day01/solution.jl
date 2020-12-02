3e4abe67d158f8fcfc91af35a7e3a82659f06a18
Andriamanitra <mikko77792@gmail.com>
2020-12-02 02:04:51 +0200

    add julia bruteforce solution

---
+++ day01/solution.jl
@@ -0,0 +1,24 @@
+using Combinatorics
+
+function brute1(numbers)
+    for nums in combinations(numbers, 2)
+        if sum(nums) == 2020
+            return prod(nums)
+        end
+    end
+end
+
+function brute2(numbers)
+    for nums in combinations(numbers, 3)
+        if sum(nums) == 2020
+            return prod(nums)
+        end
+    end
+end
+
+function main()
+    numbers = map(x -> parse(Int, x), readlines())
+    println(brute1(numbers))
+    println(brute2(numbers))
+end
+
