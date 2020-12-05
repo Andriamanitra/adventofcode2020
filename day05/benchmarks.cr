3373530432f5beb1d0f6702357aadc937c59f359
Andriamanitra <mikko77792@gmail.com>
2020-12-05 12:05:08 +0200

    add another approach for comparison

--- day05/benchmarks.cr
+++ day05/benchmarks.cr
@@ -40,15 +40,36 @@ def alt_part2(input)
   (1...SEATS).find { |seat| occupied[seat - 1] && !occupied[seat] }
 end
 
+def ss(input)
+  seats = [] of Int32
+  input.each_line { |line| seats << seat_to_id(line) }
+
+  # Credit: straight-shoota
+  seats.sort.each_cons(2, reuse: true).find { |(a, b)| a < b - 1 }.not_nil![0] + 1
+end
+
+def bs(input)
+  seats = [] of Int32
+  input.each_line { |line| seats << seat_to_id(line) }
+
+  seats.sort!
+  first = seats[0]
+  seats.bsearch_index { |x, i| x - first != i }.not_nil! + first
+end
+
 input = ARGF.gets_to_end
 p! part1(input)
 p! alt_part1(input)
 p! part2(input)
 p! alt_part2(input)
+p! ss(input)
+p! bs(input)
 
 Benchmark.ips do |x|
   x.report("original part1") { part1(input) }
-  x.report("original part2") { part2(input) }
   x.report(%("optimized" part1)) { alt_part1(input) }
+  x.report("original part2") { part2(input) }
   x.report(%("optimized" part2)) { alt_part2(input) }
+  x.report("with sorting") { ss(input) }
+  x.report("with bsearch") { bs(input) }
 end
