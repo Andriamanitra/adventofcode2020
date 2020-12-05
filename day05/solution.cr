4c7d7c80ba4dcc187b743cf71ca90408ff6f962c
Andriamanitra <mikko77792@gmail.com>
2020-12-05 07:51:06 +0200

    shorten

--- day05/solution.cr
+++ day05/solution.cr
@@ -1,5 +1,5 @@
 def seat_ids(input)
-  input.split.map { |row| row.chomp.tr("FBLR", "0101").to_i(2) }
+  input.split.map { |row| row.tr("FBLR", "0101").to_i(2) }
 end
 
 def part1(input)
@@ -8,7 +8,7 @@ end
 
 def part2(input)
   seats = seat_ids(input)
-  seats.find { |id| !seats.includes?(id + 1) && seats.includes?(id + 2) }.not_nil! + 1
+  seats.find { |id| !(id + 1).in?(seats) && (id + 2).in?(seats) }.not_nil! + 1
 rescue NilAssertionError
   "No solution"
 end
