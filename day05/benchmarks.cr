1ab9215e31cedd27b818edc31e23a63b97be66f0
Andriamanitra <mikko77792@gmail.com>
2020-12-06 05:16:18 +0200

    tiniest of optimizations

--- day05/benchmarks.cr
+++ day05/benchmarks.cr
@@ -102,7 +102,7 @@ end
 macro seat_from_ptr(ptr)
   sum = 0
   {{ptr}}.to_slice(10).each_with_index { |uint, idx|
-    sum += ((~uint & 0b0100) >> 2).to_i << (9-idx)
+    sum |= ((~uint & 0b0100) >> 2).to_i << (9-idx)
   }
   sum
 end
