db2c836303788c1b8cf708f9d3e263cfaccd1ce1
Andriamanitra <mikko77792@gmail.com>
2020-12-09 08:06:16 +0200

    solve day nine

---
+++ day09/solution.cr
@@ -0,0 +1,22 @@
+def part1(input)
+  nums = input.lines.map(&.to_i64)
+  nums[25..].zip(nums.each_cons(25)) { |target, prev25|
+    if !prev25.find { |k| prev25.reject(k).includes?(target - k) }
+      return target
+    end
+  }
+end
+
+def part2(input)
+  target = part1(input) || 556543474 # in case part1() returns nil
+  nums = input.lines.map(&.to_i64)
+  nums.each_with_index { |start, start_idx|
+    s = 0
+    summed = nums[start_idx..].take_while { |x| s += x; s < target }
+    return summed.min + summed.max if s == target
+  }
+end
+
+input = ARGF.gets_to_end
+p! part1(input)
+p! part2(input)
