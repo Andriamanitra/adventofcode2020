f0da4d836d3e461a6caa339c21ef33df3e7e9008
Andriamanitra <mikko77792@gmail.com>
2020-12-14 11:13:20 +0200

    code cleanup & get rid of array cloning

--- day14/solution.cr
+++ day14/solution.cr
@@ -26,39 +26,49 @@ def part1(input)
   mem.values.sum
 end
 
-def addresses(addr : UInt64, floating : Array(UInt64))
-  firstfloat = floating.pop
-  addr_t = addr | (1_u64 << firstfloat)
-  addr_f = addr_t - (1_u64 << firstfloat)
-  if floating.empty?
+def most_significant_bit(x : UInt64)
+  # Fill all bits after MSB:
+  x |= (x >> 1)
+  x |= (x >> 2)
+  x |= (x >> 4)
+  x |= (x >> 8)
+  x |= (x >> 16)
+  x |= (x >> 32)
+  # and now it's simple:
+  x - (x >> 1)
+end
+
+def addresses(addr : UInt64, floating : UInt64)
+  msb = most_significant_bit(floating)
+  addr_t = addr | msb
+  addr_f = addr_t - msb
+  floating -= msb
+  if floating == 0
     [addr_t, addr_f]
   else
-    addresses(addr_t, floating.clone) + addresses(addr_f, floating)
+    addresses(addr_t, floating) + addresses(addr_f, floating)
   end
 end
 
 def part2(input)
   mem = Hash(UInt64, UInt64).new
-  floating = [] of UInt64
+  floating = 0_u64
   mask = 0_u64
   input.each_line do |line|
     case line
     when .match(/mask = ([01X]+)/)
-      # puts "mask #{$1}"
       mask = $1.tr("X", "0").to_u64(2)
-      floating = $1.chars
-        .map_with_index { |ch, i| 35_u64 - i if ch == 'X' }
-        .compact
+      floating = 0_u64
+      $1.chars.each_with_index { |ch, i|
+        floating |= (MAX_BIT >> i) if ch == 'X'
+      }
     when .match(/mem\[([0-9]+)\] = ([0-9]+)/)
-      # puts "mem #{$1} #{$2}"
       m_addr = $1.to_u64 | mask
-      addresses(m_addr, floating.clone).each { |addr| mem[addr] = $2.to_u64 }
+      addresses(m_addr, floating).each { |addr| mem[addr] = $2.to_u64 }
     else
       raise "Unable to parse line: '#{line}'"
     end
   end
-  # p! mem.size
-  # mem.each { |key, value| puts "mem[#{key}]: #{value.to_s(2)} (#{value})" }
   mem.values.sum
 end
 
